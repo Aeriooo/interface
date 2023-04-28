fb62471a9a948fb6714c388cfc6a21de6697a8ca
Aeriooo <115140757+Aeriooo@users.noreply.github.com>
2023-04-28 11:39:14 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author asus
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        myclass m = new myclass();
+        m.mymethod1();
+        m.mymethod2();
+        m.mymethod3();
+        
+        
+    }
+    
+}
